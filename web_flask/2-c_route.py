09418be9ed16f88e966eae4ccc2b766248614326
Victor Mudaki Mwachi <20131567+VictorMwachi@users.noreply.github.com>
2023-01-11 22:36:34 +0300

    Update 2-c_route.py

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -5,8 +5,7 @@ The application listens on 0.0.0.0, port 5000.
 Routes:
     /: Displays 'Hello HBNB!'
     /hbnb: Displays 'HBNB'
-    /c/<text>: isplay “C ” followed by the value of the text variable\
-    (replace underscore _ symbols with a space)
+    /c/<text>: isplay “C ” followed by the value of the text variable
 """
 from flask import Flask
 
