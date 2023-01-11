b7a19e823137668334233ad9f792eedbaf117bd9
victormwachi <mudakizevictor@gmail.com>
2023-01-11 21:10:36 +0300

    rote.py

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -3,9 +3,13 @@
 a minimal flask application
 """
 from flask import flask
+
 app = flask(__name__)
+
 
 @app.route('/', strict_slashes=False)
 def hello_bnb():
     """displays hello bnb"""
     return "Hello HBNB!"
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
