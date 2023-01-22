680502136b939f0e1c3bd5c68469245964cd1c1a
Victor Mudaki Mwachi <20131567+VictorMwachi@users.noreply.github.com>
2023-01-22 16:28:33 +0300

    Update 0-setup_web_static.sh

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,18 +1,12 @@
 #!/usr/bin/env bash
-sudo apt update
-sudo apt install nginx -y
-mkdir -p /data
-mkdir -p /data/web_static
-mkdir -p /data/web_static/releases/
-mkdir -p /data/web_static/shared/
-mkdir -p /data/web_static/releases/test/
-echo "<html>
-  <head>
-    </head>
-      <body>
-          Holberton School
-	    </body>
-	    </html>">/data/web_static/releases/test/index.html
+# Sets up a web server for deployment of web_static.
+
+sudo apt-get update
+sudo apt-get install -y nginx
+
+sudo mkdir -p /data/web_static/releases/test/
+sudo mkdir -p /data/web_static/shared/
+echo "Holberton School" > /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 
 sudo chown -R ubuntu /data/
@@ -24,17 +18,21 @@ printf %s "server {
     add_header X-Served-By $HOSTNAME;
     root   /etc/nginx/html;
     index  index.html index.htm;
+
     location /hbnb_static {
-    alias /data/web_static/current;
-    index index.html index.htm;
-}   location /redirect_me {
-	return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
-	}
-	error_page 404 /404.html;
+        alias /data/web_static/current;
+        index index.html index.htm;
+    }
+
+    location /redirect_me {
+        return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
+    }
+
+    error_page 404 /404.html;
     location /404 {
-    root /etc/nginx/html;
-    internal;
-	}
+      root /etc/nginx/html;
+      internal;
+    }
 }" > /etc/nginx/sites-available/default
 
 sudo service nginx restart
