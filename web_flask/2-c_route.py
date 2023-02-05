fc9f9c9b063c0551b0969d31b30efe002a767df8
Victor Mudaki Mwachi <20131567+VictorMwachi@users.noreply.github.com>
2023-02-05 22:25:54 +0300

    Update 2-c_route.py

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -28,7 +28,7 @@ def hbnb():
 @app.route('/c/<text>', strict_slashes=False)
 def text(text):
     """displays c followed by text"""
-    return f"C {text.replace("_", " ")}"
+    return f"C {text.replace('_', ' ')}"
 
 
 if __name__ == '__main__':
